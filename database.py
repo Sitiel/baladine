06b3ae2db7a11818ccf0565175f0e31fa0a00b68
valerian <vacier@kang.fr>
2017-06-20 23:45:20 +0200

    Added env

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-engine = create_engine('postgresql://localhost/balady', convert_unicode=True)
+engine = create_engine('postgres://eoquaetdmgybuj:6b52d0331a20575e77123f24a6bae730f82447dbcf16b429b2e9674d73c33853@ec2-54-247-189-141.eu-west-1.compute.amazonaws.com:5432/d941vjvjukbn1c', convert_unicode=True)
 db_session = scoped_session(sessionmaker(autocommit=False,
                                          autoflush=False,
                                          bind=engine))
