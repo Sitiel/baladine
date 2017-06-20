66dd0d2e4281487c15ee6fd6bbdcc375037b4675
valerian <vacier@kang.fr>
2017-06-21 00:31:44 +0200

    Some debugging

--- database.py
+++ database.py
@@ -1,8 +1,17 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+import os
 
-engine = create_engine('postgres://eoquaetdmgybuj:6b52d0331a20575e77123f24a6bae730f82447dbcf16b429b2e9674d73c33853@ec2-54-247-189-141.eu-west-1.compute.amazonaws.com:5432/d941vjvjukbn1c', convert_unicode=True)
+try:
+    os.environ['DATABASE_URL']
+except NameError:
+    print "Load : " + 'postgresql://localhost/balady'
+    engine = create_engine('postgresql://localhost/balady', echo = True)
+else:
+    print "Load : " + os.environ['DATABASE_URL']
+    engine = create_engine(os.environ['DATABASE_URL'], echo = True)
+
 db_session = scoped_session(sessionmaker(autocommit=False,
                                          autoflush=False,
                                          bind=engine))
