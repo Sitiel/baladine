b9bf9b9e434b3a356c9cf25e1d12686ed15393c9
Bedoni Corentin <corentin.bedoni@imerir.com>
2017-06-21 08:55:37 +0200

    changement database.py

--- database.py
+++ database.py
@@ -3,14 +3,8 @@ from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 import os
 
-try:
-    os.environ['DATABASE_URL']
-except NameError:
-    print "Load : " + 'postgresql://localhost/balady'
-    engine = create_engine('postgresql://localhost/balady', echo = True)
-else:
-    print "Load : " + os.environ['DATABASE_URL']
-    engine = create_engine(os.environ['DATABASE_URL'], echo = True)
+print "Load : " + 'postgresql://localhost/balady'
+engine = create_engine('postgresql://postgres:imerir@localhost/balady', echo = True)
 
 db_session = scoped_session(sessionmaker(autocommit=False,
                                          autoflush=False,
