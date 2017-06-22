cb9fbe7b6dcbcde8ea8e4451411ccf910ee6dbe8
imerir <florent.courouble@imerir>
2017-06-22 17:13:47 +0200

    creation recette

---
+++ static/recette.js
@@ -0,0 +1,35 @@
+$.ajax("http://balady.herokuapp.com/ValerianKang/Balady_API/1.0.0/ingredients")
+	 .done(function(data){
+		for(var i = 0; i<data['ingredients'].length; i++){
+			$("#ingredients").append('<div id="'+i+'" class="col-sm-3" onclick="addIngr('+i+', '+data['ingredients'][i]['cout']+')">'+data['ingredients'][i]['nom']+'</div>')	
+		}
+	});
+
+ingredients = []
+var dev = 0;
+var prod = 0;
+refresh_prices()
+
+function refresh_prices(){
+	dev = Math.sqrt(ingredients.length)
+	$("#cout_dev").html(dev);
+	$("#cout_prod").html(prod);
+}
+
+function addIngr(id, cout){
+	if($("#"+id).css("color") != "rgb(0, 255, 0)"){
+		$("#"+id).css("color","rgb(0, 255, 0)");
+		ingredients.push(id);
+		prod += cout;
+		
+	}else{
+		$("#"+id).css("color","rgb(0, 0, 0)");
+		
+		var i = ingredients.indexOf(id);
+		if(i != -1) {
+			ingredients.splice(i, 1);
+		}
+		prod -= cout;
+	}
+	refresh_prices()
+}
--- static/script.js
+++ static/script.js
@@ -1,22 +1,36 @@
+var begin_day = 22;
+var begin_month = 6;
+var begin_year = 2017;
+
+
 setInterval(function (){ 
 	$.ajax("http://balady.herokuapp.com/ValerianKang/Balady_API/1.0.0/meteorology")
 	 .done(function(data){
 		var hour = data['timestamp']%24
-		console.log(data['timestamp']);
+		var day = begin_day + Math.floor(data['timestamp']/24)
+		var month = begin_month + Math.floor(day/30) ;
+		var year = begin_year + Math.floor(month/12);
+
+		day = 1+day%30;
+		month = month%12;
+
 		if(hour < 10){
 			hour = "0"+hour
 		}
+		if(day < 10){
+			day = "0"+day
+		}
+		if(month < 10){
+			month = "0"+month
+		}
+
 		$("#hour").html(hour+":00");
 		$("#weather").html(data['weather']['0']['weather']);
 		$("#futur_weather").html(data['weather']['1']['weather']);
+		$("#day").html(day+"/"+month+"/"+year
+		);
 	   });
 }, 1000);
-
-
-$("#day").html(
- Math.floor((Math.random() * 29) + 1)+"/"
- +Math.floor((Math.random() * 12) + 1)+"/2017"
-);
 
 
 $("#recettes_en_ventes").html();
