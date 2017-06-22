d29d3df91d4b8a862b77d95d027538e752bc1fa3
imerir <florent.courouble@imerir>
2017-06-22 09:53:20 +0200

    ajax climat

---
+++ static/script.js
@@ -0,0 +1,27 @@
+
+$.ajax("http://localhost:5000/ValerianKang/Balady_API/1.0.0/meteorology")
+   .done(function(data){
+	var hour = data['timestamp']%24
+    if(hour < 10){
+		hour = "0"+hour
+	}
+    $("#hour").html(hour+":00");
+    $("#weather").html(data['weather']['0']['weather']);
+	$("#futur_weather").html(data['weather']['1']['weather']);
+   });
+
+$("#day").html(
+ Math.floor((Math.random() * 29) + 1)+"/"
+ +Math.floor((Math.random() * 12) + 1)+"/2017"
+);
+
+
+$("#recettes_en_ventes").html();
+for(var i = 1; i<51; i++){
+	var price = (Math.random() * (1.0 - 0.01) + 0.01)
+	var sales = Math.floor((Math.random() * 10) + 1);
+	$("#recettes_en_ventes").append("<li>Recette "+i+" => "+(price*sales).toFixed(2)+"€ ("+sales+" verres)</li>");
+}
+
+
+$("#budget").html((Math.random() * (100 - 0.01) + 0.01).toFixed(2));
