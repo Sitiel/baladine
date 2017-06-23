ad8c519753a1fb6271f1e145d449e9cc0d604d49
imerir <florent.courouble@imerir>
2017-06-23 14:14:34 +0200

    sd

--- static/choix.js
+++ static/choix.js
@@ -8,7 +8,7 @@ function getMesRecettes() {
 			var tableau  = "Mes Recettes : <table><tr><td>Produit</td><td>Quantité</td><td>Prix</td><td>Cout production</td></tr>";
 			for (var i = 0; i< recettes_joueur.length ; i++) {
 				tableau  += "<tr><td>"+data['playerInfo']['drinksOffered'][i]['name']+"</td>";
-				tableau  += "<td>"+data['playerInfo']['drinksOffered'][i]['name']+"</td>";
+				tableau  += "<td><input type=\"number\"></td>";
 				tableau  += "<td>"+data['playerInfo']['drinksOffered'][i]['name']+"</td>";
 				tableau  += "<td>"+data['playerInfo']['drinksOffered'][i]['price']+"€</td></tr>";
 			}
